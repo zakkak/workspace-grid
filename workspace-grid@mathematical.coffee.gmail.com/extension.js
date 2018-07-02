9b4c80aada37bda7c7ef8047f003bfc266bcde05
Nikola <nikiporumbachanov@gmail.com>
2018-07-02 16:32:13 +0300

    fixed #59

--- workspace-grid@mathematical.coffee.gmail.com/extension.js
+++ workspace-grid@mathematical.coffee.gmail.com/extension.js
@@ -650,11 +650,6 @@ const ThumbnailsBox = new Lang.Class({
         }
 
         this._animatingIndicator = true;
-        let indicatorThemeNode = this._indicator.get_theme_node(),
-            indicatorTopFullBorder = indicatorThemeNode.get_padding(St.Side.TOP) + indicatorThemeNode.get_border_width(St.Side.TOP),
-            indicatorLeftFullBorder = indicatorThemeNode.get_padding(St.Side.LEFT) + indicatorThemeNode.get_border_width(St.Side.LEFT);
-        this.indicatorX = this._indicator.allocation.x1 + indicatorLeftFullBorder; // <-- added
-        this.indicatorY = this._indicator.allocation.y1 + indicatorTopFullBorder;
         Tweener.addTween(this,
                          { indicatorY: thumbnail.actor.allocation.y1,
                            indicatorX: thumbnail.actor.allocation.x1, // added
@@ -879,6 +874,10 @@ const ThumbnailsBox = new Lang.Class({
         childBox.x2 = (indicatorX2 ? indicatorX2 : (indicatorX1 + thumbnailWidth)) + indicatorLeftFullBorder;
         childBox.y1 = indicatorY1 - indicatorTopFullBorder;
         childBox.y2 = (indicatorY2 ? indicatorY2 : (indicatorY1 + thumbnailHeight)) + indicatorBottomFullBorder;
+        if (!this._animatingIndicator) {
+          this._indicatorX = indicatorX1;
+          this._indicatorY = indicatorY1;
+        }
         this._indicator.allocate(childBox, flags);
 
 
